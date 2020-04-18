3093df8ac497149b954bb6cbff9a74a3791fe432
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-04-17 20:26:23 -0400

    all crud operations fully functional for projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server= require('./server.js');
-const port= process.env.PORT || 3025;
+const port= process.env.PORT || 3075;
 
 server.listen(port, () => {
     console.log(`\n Server listening on port ${port} \n`);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet= require('helmet');
 
+const welcome_router= require('./welcome/welcome-router.js');
+const project_router= require('./projects/projects-router.js');
+
 const server= express();
 
 // built-in middleware
@@ -8,5 +11,24 @@ server.use(express.json());
 
 //third party middleware
 server.use(helmet());
+
+// route handlers
+server.use(welcome_router);
+server.use('/api/projects', project_router);
+
+// when no route handler
+server.use((req, res) => {
+	res.status(404).send(
+      `<h4 align='center'>The url ${req.url.toUpperCase()} was not found.</h4>`
+    )
+});
+
+// error handler
+server.use((err, req, res, next) => {
+	console.log('Server error:', err)
+	res.status(500).json({
+		message: "Oops, something went wrong. Please try again later.",
+	})
+})
 
 module.exports= server;
